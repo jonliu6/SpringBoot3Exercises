852e249d024bb44c681352391e7258e84eabaafa
J Liu <jonliu6@gmail.com>
2024-10-06 22:56:57 -0700

    Spring Boot 3 training exercises - 04 REST APIs data REST sample

--- 04-RESTAPIs/data/dbscripts.sql
+++ 04-RESTAPIs/data/dbscripts.sql
@@ -7,3 +7,19 @@ CREATE TABLE t_article (
   category VARCHAR(100) NOT NULL,
   content VARCHAR(2000)
 );
+
+-- DROP TABLE t_category;
+CREATE TABLE t_category (
+  id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+  category VARCHAR(100) NOT NULL,
+  parentId INT
+);
+
+INSERT INTO t_category (Category, ParentId)
+  VALUES ('Programming',0);
+INSERT INTO t_category (Category, ParentId)
+  VALUES ('Database',0);
+INSERT INTO t_category (Category, ParentId)
+  VALUES ('Java',1);
+INSERT INTO t_category (Category, ParentId)
+  VALUES ('MySQL',2);
